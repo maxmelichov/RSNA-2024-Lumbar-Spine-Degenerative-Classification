d58faf322aabde71f10b42f34d6e35782c4598f8
Maxim Melichov <80150303+maxmelichov@users.noreply.github.com>
2024-07-26 21:37:30 +0300

    Update data_loader.py

--- model_code/data_loader.py
+++ model_code/data_loader.py
@@ -5,8 +5,11 @@ import numpy as np
 import albumentations as A
 import pydicom
 import os
-
-
+from PIL import Image
+from preprocessing.segmantation_inference import SegmentaionInference, classes_of_interest, label_dict_clean
+import torch.nn.functional as F
+segmentation = SegmentaionInference(model_path=r"weights\simple_unet.pth")
+label2id = {'Normal/Mild': 0, 'Moderate':1, 'Severe':2}
 
 AUG_PROB = 0.75
 IMG_SIZE = [512, 512]
@@ -84,8 +87,27 @@ class CustomDataset(Dataset):
 
 
     @staticmethod
-    def center_crop(self, img, bboxes):
-        pass
+    def center_crop(dcm_path, bboxes):
+        min_x, max_x = 999999, -1
+        min_y, max_y = 999999, -1
+        list_of_bboxes = [boxes[0] for boxes in bboxes.values()]
+        for x, y, _, _ in list_of_bboxes:
+            min_x = min(min_x, x)
+            max_x = max(max_x, x)
+            min_y = min(min_y, y)
+            max_y = max(max_y, y)
+        original_dicom = pydicom.dcmread(dcm_path)
+        pixel_array = original_dicom.pixel_array
+        
+        # Convert DICOM pixel array to PIL image
+        image = Image.fromarray(pixel_array)
+
+        # Crop the image using the calculated bounding box
+        cropped_image_array = np.array(image.crop((min_x, min_y, max_x + 1, max_y + 1)))
+
+        # Ensure the data type matches the original DICOM's pixel array
+        cropped_image_array = cropped_image_array.astype(pixel_array.dtype)
+        return cropped_image_array
 
     @staticmethod
     def unpad_images_list(images_list, max_len):
@@ -112,12 +134,53 @@ class CustomDataset(Dataset):
             if col == "general_path_to_Axial":
                 path = sub_set[col].iloc[0]
                 list_of_files = os.listdir(path)
-
+                if len(list_of_files) < 10:
+                    list_of_files = self.pad_images_list(list_of_files, 10)
+                elif len(list_of_files) > 10:
+                    list_of_files = self.unpad_images_list(list_of_files, 10)
                 for i, file in enumerate(list_of_files):
                     dcm = pydicom.dcmread(os.path.join(path, file))
                     Axial_T2[..., i] = dcm.pixel_array
+            elif col == "general_path_to_Sagittal_T1":
+                path = sub_set[col].iloc[0]
+                list_of_files = os.listdir(path)
+                middle_index = len(list_of_files) // 2
+                bboxes = segmentation.inference(list_of_files[middle_index])
+                if len(list_of_files) < 15:
+                    list_of_files = self.pad_images_list(list_of_files, 15)
+                elif len(list_of_files) > 15:
+                    list_of_files = self.unpad_images_list(list_of_files, 15)
 
+                for i, file in enumerate(list_of_files):
+                    self.center_crop(os.path.join(path, file), bboxes)
+                    Sagittal_T1[..., i] = dcm.pixel_array
+            elif col == "general_path_to_Sagittal_T2_STIR":
+                path = sub_set[col].iloc[0]
+                list_of_files = os.listdir(path)
+                middle_index = len(list_of_files) // 2
+                bboxes = segmentation.inference(list_of_files[middle_index])
+                if len(list_of_files) < 15:
+                    list_of_files = self.pad_images_list(list_of_files, 15)
+                elif len(list_of_files) > 15:
+                    list_of_files = self.unpad_images_list(list_of_files, 15)
+
+                for i, file in enumerate(list_of_files):
+                    self.center_crop(os.path.join(path, file), bboxes)
+                    Sagittal_T2_STIR[..., i] = dcm.pixel_array
         
         if self.transform:
-            pass
-        pass
+            Axial_T2 = self.transform(image=Axial_T2)['image']
+            Sagittal_T1 = self.transform(image=Sagittal_T1)['image']
+            Sagittal_T2_STIR = self.transform(image=Sagittal_T2_STIR)['image']
+        
+        Axial_T2 = torch.tensor(Axial_T2).transpose(2, 0, 1)
+        Sagittal_T1 = torch.tensor(Sagittal_T1).transpose(2, 0, 1)
+        Sagittal_T2_STIR = torch.tensor(Sagittal_T2_STIR).transpose(2, 0, 1)
+        category_hot = F.one_hot(torch.tensor(category), num_classes=5)
+        labels = torch.tensor([label2id[self.df_labels.iloc[index]['spinal_canal_stenosis']],
+                              label2id[self.df_labels.iloc[index]['left_neural_foraminal_narrowing']],
+                              label2id[self.df_labels.iloc[index]['right_neural_foraminal_narrowing']],
+                              label2id[self.df_labels.iloc[index]['left_subarticular_stenosis']],
+                                label2id[self.df_labels.iloc[index]['right_subarticular_stenosis']]], dtype=torch.float32)
+
+        return Axial_T2, Sagittal_T1, Sagittal_T2_STIR, category_hot, labels
